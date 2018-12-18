9a74bee7a2bb1bc4a65e0ddc45f9ba954ae6239b
Chaeoh <limchyo>
2018-12-18 18:01:54 +0900

    댓글 추가, 승인, 삭제, 포스트 글 발행 view 작성

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,8 +1,8 @@
 # from django.contrib.urls import url
 from django.urls import path
 from .views import (AboutView, PostListView, PostDetailView, CreatePostView,
-                    PostUpdateView, PostDeleteView, DraftListView,
-)
+PostUpdateView, PostDeleteView, DraftListView), (add_comment_to_post,
+comment_approve, comment_remove, post_publish)
 
 urlpatterns = [
     path('/', PostListView.as_view(), name='post_list'),
@@ -12,4 +12,8 @@ urlpatterns = [
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/remove/', PostDeleteView.as_view(), name='post_remove'),
     path('drafts/', DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/comment/', add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', comment_remove, name='comment_remove'),
+    path('post/<int:pk>/publish/', post_publish, name='post_publish'),
 ]
